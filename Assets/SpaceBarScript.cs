a240bbd312a75eb0f1f3bae242c73837107d6a5a
Martin_Burs <playmartu18@gmail.com>
2024-06-19 15:25:45 -0300

    version clase 1

---
+++ Assets/SpaceBarScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBarScript : MonoBehaviour
+{
+    public GameObject[] figuras;
+    public int indiceArray = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int k = 0; k< figuras.Length; k++)
+        {
+            figuras[k].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            figuras[indiceArray].SetActive(false);
+            indiceArray++;
+            if(indiceArray == figuras.Length)
+            {
+                indiceArray = 0;
+            }
+            figuras[indiceArray].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            figuras[indiceArray].SetActive(false);
+            indiceArray++;
+            if(indiceArray == -1)
+            {
+                indiceArray = figuras.Length - 1;
+            }
+            figuras[indiceArray].SetActive(true);
+        }
+    }
+}
